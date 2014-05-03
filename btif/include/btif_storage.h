c402d931af32828a3e1e682ee99c407b75355da5
Schischu <schischu65@gmail.com>
2014-05-03 14:43:52 +0200

    bt: Add wiimote pairing support
    Change-Id: Ie1e9fa445cc1967679f3d74277df4c594fca5330
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- btif/include/btif_storage.h
+++ btif/include/btif_storage.h
@@ -296,6 +296,17 @@ BOOLEAN btif_storage_is_restricted_device(const bt_bdaddr_t *remote_bd_addr);
 **
 *******************************************************************************/
 int btif_storage_get_num_bonded_devices(void);
+/*******************************************************************************
+** Function         btif_storage_is_wiimote
+**
+** Description      BTIF storage API - checks if this device is a wiimote
+**
+** Returns          TRUE   if the device is found in wiimote device list
+**                  FALSE otherwise
+**
+*******************************************************************************/
+BOOLEAN btif_storage_is_wiimote(bt_bdaddr_t *remote_bd_addr, bt_bdname_t *remote_bd_name);
+
 #if (BLE_INCLUDED == TRUE)
 bt_status_t btif_storage_add_ble_bonding_key( bt_bdaddr_t *remote_bd_addr,
                                               char *key,
