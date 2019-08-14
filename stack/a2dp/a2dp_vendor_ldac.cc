dace0505ee11305ee45890d64dd02e15ddd6a865
Jackeagle <jackeagle102@gmail.com>
2019-08-14 03:34:14 -0400

    Revert "Explicit SBC Dual Channel (SBC HD) support"
    This reverts commit c6436f0ec8a809e89e1d6421d58f30f0faa38c03.

--- stack/a2dp/a2dp_vendor_ldac.cc
+++ stack/a2dp/a2dp_vendor_ldac.cc
@@ -815,7 +815,6 @@ static bool select_audio_channel_mode(
         return true;
       }
       break;
-    case BTAV_A2DP_CODEC_CHANNEL_MODE_DUAL_CHANNEL:
     case BTAV_A2DP_CODEC_CHANNEL_MODE_NONE:
       break;
   }
@@ -1094,7 +1093,6 @@ bool A2dpCodecConfigLdac::setCodecConfig(const uint8_t* p_peer_codec_info,
         break;
       }
       break;
-    case BTAV_A2DP_CODEC_CHANNEL_MODE_DUAL_CHANNEL:
     case BTAV_A2DP_CODEC_CHANNEL_MODE_NONE:
       codec_capability_.channel_mode = BTAV_A2DP_CODEC_CHANNEL_MODE_NONE;
       codec_config_.channel_mode = BTAV_A2DP_CODEC_CHANNEL_MODE_NONE;
